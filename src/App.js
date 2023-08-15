1e5d6f3dffc44c222fbe5f551c2f362b0f58ff7e
Juan Fernando <juanfernandodemourasilva@gmail.com>
2023-08-15 12:23:56 -0300

    pronto

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
+import TodoWrapper from "./components/TodoWrapper";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <TodoWrapper  />
+    </div>
+  );
 }
 
 export default App;
--- src/components/EditTodoForm.js
+++ src/components/EditTodoForm.js
@@ -10,7 +10,7 @@ export const EditTodoForm = ({editTodo, task}) => {
   return (
     <form onSubmit={handleSubmit} className="TodoForm">
     <input type="text" value={value} onChange={(e) => setValue(e.target.value)} className="todo-input" placeholder='Update task' />
-    <button type="submit" className='todo-btn'>Add Task</button>
+    <button type="submit" className='todo-btn'>Terminar mudança</button>
   </form>
   )
 }
